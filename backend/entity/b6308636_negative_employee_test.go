020ae716e6e4d8697d28b09c8a18c9ace6057f26
non-nattawut <nattavutnon@gmail.com>
2023-02-15 14:06:29 -0800

    negative employee ID case - close #4

---
+++ backend/entity/b6308636_negative_employee_test.go
@@ -0,0 +1,76 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/onsi/gomega"
+)
+
+func TestNegativeEmployeeID(t *testing.T) {
+	g := gomega.NewGomegaWithT(t)
+
+	t.Run("Negative Employee ID Case 1", func(t *testing.T) {
+		e := Employee{
+			Name:       "nattawut Rodthong",
+			Email:      "nattavutnon@gmail.com",
+			EmployeeID: "12345678",
+		}
+
+		ok, err := govalidator.ValidateStruct(e)
+
+		g.Expect(ok).NotTo(gomega.BeTrue())
+
+		g.Expect(err).NotTo(gomega.BeNil())
+
+		g.Expect(err.Error()).To(gomega.Equal("Bad employee ID format"))
+	})
+
+	t.Run("Negative Employee ID Case 2", func(t *testing.T) {
+		e := Employee{
+			Name:       "nattawut Rodthong",
+			Email:      "nattavutnon@gmail.com",
+			EmployeeID: "A12345678",
+		}
+
+		ok, err := govalidator.ValidateStruct(e)
+
+		g.Expect(ok).NotTo(gomega.BeTrue())
+
+		g.Expect(err).NotTo(gomega.BeNil())
+
+		g.Expect(err.Error()).To(gomega.Equal("Bad employee ID format"))
+	})
+
+	t.Run("Negative Employee ID Case 3", func(t *testing.T) {
+		e := Employee{
+			Name:       "nattawut Rodthong",
+			Email:      "nattavutnon@gmail.com",
+			EmployeeID: "M12345678123123",
+		}
+
+		ok, err := govalidator.ValidateStruct(e)
+
+		g.Expect(ok).NotTo(gomega.BeTrue())
+
+		g.Expect(err).NotTo(gomega.BeNil())
+
+		g.Expect(err.Error()).To(gomega.Equal("Bad employee ID format"))
+	})
+
+	t.Run("Negative Employee ID Case 4", func(t *testing.T) {
+		e := Employee{
+			Name:       "nattawut Rodthong",
+			Email:      "nattavutnon@gmail.com",
+			EmployeeID: "S12",
+		}
+
+		ok, err := govalidator.ValidateStruct(e)
+
+		g.Expect(ok).NotTo(gomega.BeTrue())
+
+		g.Expect(err).NotTo(gomega.BeNil())
+
+		g.Expect(err.Error()).To(gomega.Equal("Bad employee ID format"))
+	})
+}
--- backend/entity/schema.go
+++ backend/entity/schema.go
@@ -8,5 +8,5 @@ type Employee struct {
 	gorm.Model
 	Name       string `valid:"required~Name cannot Blank"`
 	Email      string
-	EmployeeID string `valid:"matches(^([JMS][0-9]{8})$)~Bad format"`
+	EmployeeID string `valid:"matches(^([JMS][0-9]{8})$)~Bad employee ID format"`
 }
